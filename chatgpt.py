4689a24d406b4ffa1d9f4671a597e50b0b1b2752
Otso <87015957+otsoniemi@users.noreply.github.com>
2022-12-19 10:50:46 +0200

    Code Improvements.

--- chatgpt.py
+++ chatgpt.py
@@ -1,11 +1,12 @@
 import requests
 import json
 
+def get_response(text, lang):
+    params = {'text': text, 'lang': lang}
+    response = requests.get('https://api.pawan.krd/chat/gpt', params=params)
+    return response.json()
+
 while True:
-    message = str(input('Message: '))
-    def get_response(text, lang):
-        params = {'text': text, 'lang': lang}
-        response = requests.get('https://api.pawan.krd/chat/gpt', params=params)
-        return response.json()
+    message = input('Message: ')  # str() is not necessary here
     response = get_response(message, 'en')
     print(response['reply'])
