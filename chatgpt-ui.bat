c7954b8cb65d1aae868d19b1c5b9521370fa77c4
Otso <87015957+otsoniemi@users.noreply.github.com>
2022-12-20 14:09:53 +0200

    Update chatgpt-ui.bat

--- chatgpt-ui.bat
+++ chatgpt-ui.bat
@@ -1,3 +1,16 @@
 @echo off
-py "%cd%\chatgpt.py"
+
+rem Change to the script's directory
+cd /d "%~dp0"
+
+rem Set the Python executable path
+set PYTHON_EXE=python
+
+rem Set the Python script path
+set SCRIPT_PATH=chatgpt.py
+
+rem Run the Python script
+%PYTHON_EXE% %SCRIPT_PATH%
+
+rem Pause the command prompt window
 pause
